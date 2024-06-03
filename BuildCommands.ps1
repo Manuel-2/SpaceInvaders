c87d7da43a24ad8731ab1c5a552d83fb4ed9b548
Manuel Peñaloza <25lopezm25@gmail.com>
2024-06-02 21:29:40 -0700

    se escribieron unas funciones en un script para windows powerShell, hacen mas comodo compilar y probar el juego

---
+++ BuildCommands.ps1
@@ -0,0 +1,11 @@
+# function definitions, you can copy an paste in the terminal and use them
+function play {
+    scons platform=windows
+    # make sure to have Godot_v4.1.4-stable_win64_console.exe in path or put the absolute path, also change
+    Godot_v4.1.4-stable_win64_console.exe --path .\project
+}
+
+function editor {
+    scons platform=windows
+    Godot_v4.1.4-stable_win64_console.exe -e --path .\project
+}
